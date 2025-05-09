b3f2817fa439335ccf664398bec4ff261d2c67ed
XaerickSReborn <u202213358@upc.edu.pe>
2025-05-08 20:45:23 -0500

    feat(i18n): configure language files.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import 'primeicons/primeicons.css';
 
-createApp(App).mount('#app')
+
+import { createI18n } from 'vue-i18n';
+import englishMessages from './assets/i18n/en.json'; 
+import spanishMessages from './assets/i18n/es.json';
+const i18n = createI18n({
+  legacy: false,
+  locale: 'en',
+  fallbackLocale: 'es',
+  messages: {
+    en: englishMessages,
+    es: spanishMessages
+  }
+});
+
+const app = createApp(App);
+app.use(PrimeVue, { ripple: true });
+app.use(i18n);
+
+app.mount('#app');
