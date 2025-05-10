95c08b9e6508bfd1ce8a768f169cd4907867c43f
XaerickSReborn <u202213358@upc.edu.pe>
2025-05-09 21:35:59 -0500

    feat(router): add router index

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './style.css'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import 'primeicons/primeicons.css';
-
+import router from "./router/index.js";
 
 import { createI18n } from 'vue-i18n';
 import englishMessages from './assets/i18n/en.json'; 
@@ -21,5 +21,6 @@ const i18n = createI18n({
 const app = createApp(App);
 app.use(PrimeVue, { ripple: true });
 app.use(i18n);
+app.use(router);
 
 app.mount('#app');
