bed0fb0c0cf018dde766b30a0df6bb8f8647906b
XaerickSReborn <u202213358@upc.edu.pe>
2025-05-12 00:51:27 -0500

    feat: add dashboard component and PrimeVue components

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import 'primeicons/primeicons.css';
 import router from "./router/index.js";
+import Button from 'primevue/button'
+import Toolbar from 'primevue/toolbar'
+import Avatar from 'primevue/avatar'
+import Select from 'primevue/select';
+
 
 import { createI18n } from 'vue-i18n';
 import englishMessages from './assets/i18n/en.json'; 
@@ -22,5 +27,9 @@ const app = createApp(App);
 app.use(PrimeVue, { ripple: true });
 app.use(i18n);
 app.use(router);
+app.component('pv-button', Button);
+app.component('pv-toolbar', Toolbar);
+app.component('pv-avatar', Avatar);
+app.component('pv-select', Select);
 
 app.mount('#app');
