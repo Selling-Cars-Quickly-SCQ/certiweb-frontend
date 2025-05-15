4c0077edec192ac79c0ba66f042001f78046b8a5
XaerickSReborn <u202213358@upc.edu.pe>
2025-05-15 05:53:26 -0500

    feat: add new PrimeVue components and ToastService

--- src/main.js
+++ src/main.js
@@ -17,6 +17,11 @@ import Select from 'primevue/select';
 import Checkbox from 'primevue/checkbox';
 import CheckboxGroup from 'primevue/checkboxgroup';
 import Dialog from 'primevue/dialog';
+import FileUpload from 'primevue/fileupload';
+import DatePicker from 'primevue/datepicker';
+import ToastService from 'primevue/toastservice';
+import Toast from 'primevue/toast';
+import ProgressSpinner from 'primevue/progressspinner';
 
 import { createI18n } from 'vue-i18n';
 import englishMessages from './assets/i18n/en.json'; 
@@ -33,6 +38,7 @@ const i18n = createI18n({
 
 const app = createApp(App);
 app.use(PrimeVue);
+app.use(ToastService);
 app.component('pv-inputText', InputText);
 app.component('pv-password', Password);
 app.component('pv-selectButton', SelectButton);
@@ -42,11 +48,15 @@ app.component('pv-steps', Steps);
 app.component('pv-checkbox', Checkbox);
 app.component('pv-checkboxgroup', CheckboxGroup);
 app.component('pv-dialog', Dialog);
-app.use(i18n);
-app.use(router);
+app.component('pv-fileupload', FileUpload);
+app.component('pv-datePicker', DatePicker);
+app.component('pv-toast', Toast);
 app.component('pv-button', Button);
 app.component('pv-toolbar', Toolbar);
 app.component('pv-avatar', Avatar);
 app.component('pv-select', Select);
+app.component('pv-progress-spinner', ProgressSpinner);
 
+app.use(router);
+app.use(i18n);
 app.mount('#app');
