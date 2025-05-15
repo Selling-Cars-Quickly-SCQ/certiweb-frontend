07164163133dc003c79937ad5e5120fa37a909a9
XaerickSReborn <u202213358@upc.edu.pe>
2025-05-15 05:56:37 -0500

    feat(model): add reservation entity class

---
+++ src/certifications/model/reservation.entity.js
@@ -0,0 +1,12 @@
+export class Reservation {
+    constructor() {
+        this.reservationName = "";
+        this.imageUrl = "";
+        this.brand = "";
+        this.model = "";
+        this.inspectionDateTime = "";
+        this.price = "";
+        this.status = "";
+        this.userId = "";
+    }
+}
