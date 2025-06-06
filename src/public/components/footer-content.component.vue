b5dd1e3b9589a1224fdffcb5d906395eb015ddfd
XaerickSReborn <u202213358@upc.edu.pe>
2025-06-05 20:17:26 -0500

    feat(footer): add navigation to terms of use page

--- src/public/components/footer-content.component.vue
+++ src/public/components/footer-content.component.vue
@@ -1,5 +1,11 @@
 <script setup>
 import certiwebLogo from '@/assets/certiweb.png'
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const navigateToTerms = () => {
+    router.push(`/terms-of-use`);
+  };
 </script>
 
 <template>
@@ -11,8 +17,8 @@ import certiwebLogo from '@/assets/certiweb.png'
       <div class="footer-text">
         © 2025 Certiweb.com. Reservados todos los derechos.
       </div>
-      <div class="footer-link">
-        <a href="#" class="footer-terms">
+      <div class="footer-link" @click="navigateToTerms('/terms-of-use')">
+        <a class="footer-terms">
           Términos y Condiciones
         </a>
       </div>
